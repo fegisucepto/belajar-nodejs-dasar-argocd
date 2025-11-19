c440898803ad05685cee6685f4ce7bbf19f40ad2
fegisucepto <fegisucepto@gmail.com>
2025-11-19 12:57:25 +0700

    Fix SonarQube configuration and disable automatic analysis

--- sonar-project.js
+++ sonar-project.js
@@ -2,19 +2,20 @@ const scanner = require('sonarqube-scanner').default;
 
 scanner(
   {
-    serverUrl: 'https://sonarcloud.io',  // Perbaiki URL-nya
+    serverUrl: 'https://sonarcloud.io',
     token: process.env.SONAR_TOKEN || '',
     options: {
       'sonar.organization': 'fegisucepto',
       'sonar.projectKey': 'fegisucepto_belajar-nodejs-dasar',
       'sonar.projectName': 'belajar-nodejs-dasar',
-      'sonar.sources': '.',
+      'sonar.sources': 'src',
       'sonar.sourceEncoding': 'UTF-8',
       'sonar.exclusions': 'node_modules/**,**/*.test.js,**/*.spec.js,coverage/**,dist/**,**/__tests__/**,**/test/**',
       'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info',
       'sonar.scm.disabled': 'true',
-      'sonar.qualitygate.wait': 'true'
-    },
+      'sonar.qualitygate.wait': 'true',
+      'sonar.analysis.ci': 'true'  // Menandakan ini adalah analisis CI
+    }
   },
   (result) => {
     console.log('SonarQube analysis finished');
