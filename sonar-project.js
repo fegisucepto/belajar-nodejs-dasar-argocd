2c03d20b1c4b92157476cf4393a3618620a96169
fegisucepto <fegisucepto@gmail.com>
2025-11-19 12:07:28 +0700

    Fix SonarQube configuration and separate workflow

--- sonar-project.js
+++ sonar-project.js
@@ -4,15 +4,12 @@ scanner(
   {
     serverUrl: 'https://sonarcloud.io',
     options: {
-      'sonar.organization': 'fegisucepto', // Ganti dengan organization key Anda
-      'sonar.projectKey': 'fegisucepto_belajar-nodejs-dasar', // Ganti dengan project key unik
+      'sonar.organization': 'fegisucepto',
+      'sonar.projectKey': 'fegisucepto_belajar-nodejs-dasar',
       'sonar.projectName': 'belajar-nodejs-dasar',
       'sonar.sources': '.',
-      'sonar.tests': '__tests__',
-      'sonar.test.inclusions': '**/*.test.js,**/*.spec.js',
-      'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info',
       'sonar.sourceEncoding': 'UTF-8',
-      'sonar.exclusions': 'node_modules/**,**/*.test.js,**/*.spec.js,coverage/**,dist/**,**/__tests__/**',
+      'sonar.exclusions': 'node_modules/**,**/*.test.js,**/*.spec.js,coverage/**,dist/**,**/__tests__/**,**/test/**',
       'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info',
       'sonar.testExecutionReportPaths': 'test-report.xml'
     },
