30a6c8eea800fff085c22843c10472f9a261085d
fegisucepto <fegisucepto@gmail.com>
2025-11-19 12:09:44 +0700

    Fix SonarQube configuration and disable automatic analysis

--- sonar-project.js
+++ sonar-project.js
@@ -11,7 +11,10 @@ scanner(
       'sonar.sourceEncoding': 'UTF-8',
       'sonar.exclusions': 'node_modules/**,**/*.test.js,**/*.spec.js,coverage/**,dist/**,**/__tests__/**,**/test/**',
       'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info',
-      'sonar.testExecutionReportPaths': 'test-report.xml'
+      'sonar.testExecutionReportPaths': 'test-report.xml',
+      'sonar.scm.disabled': 'true',  // Nonaktifkan SCM jika tidak diperlukan
+      'sonar.scm.provider': 'git',    // Atau 'svn' jika menggunakan SVN
+      'sonar.scm.exclusions.disabled': 'true'  // Nonaktifkan pengecualian SCM
     },
   },
   () => process.exit()
